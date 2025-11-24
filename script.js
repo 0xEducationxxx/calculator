df30ebf357bad923cafb1273a900b5534355d1a4
0xm7d <simobriti12@gmail.com>
2025-11-24 11:26:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var display = document.getElementById("input");
+function appendValue(val) {
+    if (display.value === "0") display.value = "";
+    display.value += val;
+}
+function clearAll() {
+    display.value = "0";
+}
+function calculate() {
+    var exp = display.value;
+    if (exp.endsWith('+') || exp.endsWith('-') || exp.endsWith('*') || exp.endsWith('/')) {
+        display.value = "Error";
+        return;
+    }
+    display.value = eval(exp);
+}
